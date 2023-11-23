01162ac97b5bc7bfd2f13fd218fdc0dcc5dd98f0
antoine-paradigmes <antoine@paradigmes.studio>
2023-11-23 16:10:34 +0100

    update rental room link

--- footer.php
+++ footer.php
@@ -10,7 +10,7 @@
 </footer>
 <footer class="footer-location">
   <h2 class="accroche has-text-align-center">Psst ! La péniche a aussi son gîte !</h2>
-  <a href="https://www.sejour-insolite-peniche-lille.com/" class="cta cta-footer cta-decoration" target="_blank">Je passe la nuit sur la péniche
+  <a href="https://www.conciergerie-lilloise.com/property/peniche-citadelle-coeur-de-lille/" class="cta cta-footer cta-decoration" target="_blank">Je passe la nuit sur la péniche
   </a>
 </footer>
 
